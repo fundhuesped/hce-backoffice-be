34596e45629882374bdab088617f8081dec37049
Nicolas Gonzalez <nicolas@ingenia.la>
2018-12-06 11:16:43 -0300

    feat(visit): fixed type

--- hc_hce/views/visit_view.py
+++ hc_hce/views/visit_view.py
@@ -77,9 +77,11 @@ class VisitDetails(generics.RetrieveUpdateDestroyAPIView):
                 serializer = self.get_serializer(instance)
                 return Response(serializer.data)
             else:
-                if instance.state == Visit.STATE_OPEN
+                if instance.state == Visit.STATE_OPEN:
                     instance.state = Visit.STATE_CLOSED
                     instance.save();
+                    return Response('Cerrada automaticamente luego de 8 horas', status=status.HTTP_400_BAD_REQUEST)
+                else:
                     return Response('Solo se pueden modificar dentro de las 8 horas', status=status.HTTP_400_BAD_REQUEST)
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
