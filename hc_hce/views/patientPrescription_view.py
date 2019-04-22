b5e29720a0de03f09830fa68305f5c23ba182f27
Nicolas Gonzalez <nicolas@ingenia.la>
2019-04-22 17:21:25 -0300

    fix(prescriptions): fixed issueDate when creating multiple prescriptions

--- hc_hce/views/patientPrescription_view.py
+++ hc_hce/views/patientPrescription_view.py
@@ -16,7 +16,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.http import Http404
 from django.core.exceptions import ObjectDoesNotExist
-from datetime import timedelta  
+from datetime import timedelta
 from datetime import datetime
 
 class PatientPrescriptionList(PaginateListCreateAPIView):
@@ -34,6 +34,10 @@ class PatientPrescriptionList(PaginateListCreateAPIView):
         startDate = self.request.query_params.get('startDate')
         if startDate is not None:
             queryset = queryset.filter(issuedDate_gte=startDate)
+
+        not_state = self.request.query_params.get('notState')
+        if not_state is not None:
+            queryset = queryset.exclude(state=not_state)
 
         endDate = self.request.query_params.get('endDate')
         if endDate is not None:
@@ -57,7 +61,7 @@ class PatientPrescriptionList(PaginateListCreateAPIView):
         # TODO Remove to new class
 
         visits = Visit.objects.filter(paciente=patient_id, profesional=profesional.id, status=Visit.STATUS_ACTIVE, state=Visit.STATE_OPEN)
-        
+
         if visits.count()==0:
             paciente = Paciente.objects.filter(pk=patient_id).get()
             Visit.objects.create(
@@ -71,7 +75,7 @@ class PatientPrescriptionList(PaginateListCreateAPIView):
             originalDate = data['issuedDate']
 
             for x in xrange(0,data['cantRecetas']):
-                data['issuedDate'] = datetime.strptime(originalDate, "%Y-%m-%d").date() + timedelta(days=28*(x+1))  
+                data['issuedDate'] = datetime.strptime(originalDate, "%Y-%m-%d").date() + timedelta(days=28*(x))
 
                 if data['prescripctionType'] == 'Arv':
                     serializer = PatientARVPrescriptionNestSerializer(data=data, context={'request': request})
@@ -79,21 +83,21 @@ class PatientPrescriptionList(PaginateListCreateAPIView):
                     serializer = PatientPrescriptionNestSerializer(data=data, context={'request': request})
                 serializer.is_valid(raise_exception=True)
                 self.perform_create(serializer)
-                
+
                 prescriptionsIds.append(serializer.data['id'])
             return Response({"prescriptionsIds":prescriptionsIds})
 
         else:
-            
+
             if data['prescripctionType'] == 'Arv':
                 serializer = PatientARVPrescriptionNestSerializer(data=data, context={'request': request})
-            
+
             elif data['prescripctionType'] == 'Vaccine':
                 serializer = PatientVaccinePrescriptionNestSerializer(data=data, context={'request': request})
-           
+
             else:
                 serializer = PatientPrescriptionNestSerializer(data=data, context={'request': request})
-           
+
             serializer.is_valid(raise_exception=True)
             self.perform_create(serializer)
         return Response(serializer.data)
